590c7d5950ba8cde917ac8259ca4ea4cf89f9633
khdoba <kholboevdostonbek@gmail.com>
2022-11-01 11:09:13 +0500

    monorepo init

---
+++ Earthfile
@@ -0,0 +1,10 @@
+VERSION 0.6
+
+all-unit-test:
+    BUILD ./libs/hello+unit-test
+    BUILD ./services/one+unit-test
+    BUILD ./services/two+unit-test
+
+all-docker:
+    BUILD ./services/one+docker
+    BUILD ./services/two+docker
---
+++ services/two/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "net/http"
+
+  "monorepo_test/libs/hello"
+  "github.com/labstack/echo/v4"
+)
+
+func main() {
+  e := echo.New()
+  e.GET("/two/hello", func(c echo.Context) error {
+    return c.String(http.StatusOK, hello.Greet("World"))
+  })
+  _ = e.Start(":8080")
+}
