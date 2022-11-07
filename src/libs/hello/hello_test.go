1fc78fad760dafb7a152254a35f3a1f09e371997
Dostonbek <kholboevdostonbek@gmail.com>
2022-11-07 12:12:09 +0500

    Hello lib changed, test scripts updated to run all packages in a folder

--- src/libs/hello/hello_test.go
+++ src/libs/hello/hello_test.go
@@ -1,11 +1,16 @@
 package hello
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGreet(t *testing.T) {
 	expected := "Hello, Earth!"
 	actual := Greet("Earth")
 	if expected != actual {
+		fmt.Println("expected", expected)
+		fmt.Println("actual", string(actual))
 		t.Fail()
 	}
 }
