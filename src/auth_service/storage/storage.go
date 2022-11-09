530e3c927333fe952a1e3dc20bf755f71af501be
bekhzod <bekhzod.asadullayev@gmail.com>
2022-11-09 13:03:31 +0500

    storage created

--- src/auth_service/storage/storage.go
+++ src/auth_service/storage/storage.go
@@ -1,9 +1,10 @@
 package storage
 
 import (
-	"github.com/jmoiron/sqlx"
 	"monorepo/src/auth_service/storage/postgres"
 	"monorepo/src/auth_service/storage/repo"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type IStorage interface {
@@ -11,13 +12,11 @@ type IStorage interface {
 }
 
 type storagePg struct {
-	db       *sqlx.DB
 	authRepo repo.IAuthStorage
 }
 
 func New(db *sqlx.DB) *storagePg {
 	return &storagePg{
-		db:       db,
 		authRepo: postgres.New(db),
 	}
 }
