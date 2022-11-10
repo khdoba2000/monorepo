da2a09b7b61d0247a4d4e33988956b79110c4f3a
NeverlandMJ <khasanovasumbula@gmail.com>
2022-11-10 18:12:39 +0500

    hashing password in api-gateway

--- src/api_gateway/handlers/auth_handler/auth_handle.go
+++ src/api_gateway/handlers/auth_handler/auth_handle.go
@@ -2,11 +2,13 @@ package auth_handler
 
 import (
 	"context"
+	"fmt"
 	"monorepo/src/api_gateway/configs"
 	"monorepo/src/api_gateway/dependencies"
 	"monorepo/src/api_gateway/models"
 	u "monorepo/src/api_gateway/utils"
 	"monorepo/src/idl/auth_service"
+	"monorepo/src/libs/etc"
 	"monorepo/src/libs/log"
 	libsUtils "monorepo/src/libs/utils"
 	"net/http"
@@ -101,6 +103,13 @@ func (ah *authHandler) ResetPassword(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	hashedPassword, err := etc.GeneratePasswordHash(body.NewPassword)
+	if err != nil {
+		u.HandleInternalWithMessage(w, err, "error occured whiling hashing new password")
+	}
+
+	// this is just avoid unused variable error. Later we will send it to service where this hashed password will be saved to database
+	fmt.Println(hashedPassword)
 	// here we should update password from databse
 
 	w.WriteHeader(http.StatusOK)
