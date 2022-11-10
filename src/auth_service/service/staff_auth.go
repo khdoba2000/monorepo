e36cdf84cefa03ffe0b55e3c0d8e5e8eb07c4a15
Dostonbek <kholboevdostonbek@gmail.com>
2022-11-10 16:52:57 +0500

    tracer lines commented out

--- src/auth_service/service/staff_auth.go
+++ src/auth_service/service/staff_auth.go
@@ -7,14 +7,12 @@ import (
 	"monorepo/src/auth_service/storage"
 	pb "monorepo/src/idl/auth_service"
 	"monorepo/src/libs/log"
-	"time"
 
 	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
 	"go.uber.org/zap"
 )
 
@@ -30,46 +28,36 @@ func New(storage storage.IStorage, logger log.Factory, tracer opentracing.Tracer
 	return &AuthService{
 		storage: storage,
 		logger:  logger,
+		tracer:  tracer,
 	}
 }
 
 func (as *AuthService) StaffLogin(ctx context.Context, req *pb.StaffLoginRequest) (*pb.AuthResponse, error) {
 
-	as.logger.For(ctx).Info("StaffLogin req", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
-	if span := opentracing.SpanFromContext(ctx); span != nil {
-		span := as.tracer.StartSpan("Query database", opentracing.ChildOf(span.Context()))
-		span.SetTag("param.phoneNumber", req.PhoneNumber)
-		span.SetTag("param.username", req.Username)
-		ext.SpanKindRPCClient.Set(span)
-		defer span.Finish()
-		ctx = opentracing.ContextWithSpan(ctx, span)
-	}
-	//simulate signup reg
-	time.Sleep(1 * time.Second)
+	as.logger.For(ctx).Info("StaffLogin started", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
 
 	res, err := as.storage.Authenitication().StaffLogin(*req)
 	if err != nil {
 		as.logger.For(ctx).Error("failed to staff login username or password didn't match", zap.Error(err))
 		return nil, status.Error(codes.Unauthenticated, "username or password is incorrect")
 	}
+	as.logger.For(ctx).Info("StaffLogin finished", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
+
 	return &res, nil
 }
 
 func (as *AuthService) StaffSignUp(ctx context.Context, req *pb.StaffSignUpRequest) (*pb.AuthResponse, error) {
 
-	as.logger.For(ctx).Info("StaffSignUp req", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
-	if span := opentracing.SpanFromContext(ctx); span != nil {
-		span := as.tracer.StartSpan("Query database", opentracing.ChildOf(span.Context()))
-		span.SetTag("param.phoneNumber", req.PhoneNumber)
-		span.SetTag("param.username", req.Username)
-		ext.SpanKindRPCClient.Set(span)
-		defer span.Finish()
-		ctx = opentracing.ContextWithSpan(ctx, span)
-	}
-	//simulate signup reg
-	time.Sleep(1 * time.Second)
+	as.logger.For(ctx).Info("StaffSignUp started", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
+	// if span := opentracing.SpanFromContext(ctx); span != nil {
+	// 	span := as.tracer.StartSpan("Query database", opentracing.ChildOf(span.Context()))
+	// 	span.SetTag("param.phoneNumber", req.PhoneNumber)
+	// 	span.SetTag("param.username", req.Username)
+	// 	ext.SpanKindRPCClient.Set(span)
+	// 	defer span.Finish()
+	// 	ctx = opentracing.ContextWithSpan(ctx, span)
+	// }
 
-	var res pb.AuthResponse
 	res, err := as.storage.Authenitication().StaffSignUp(*req)
 
 	if errors.Is(err, bcrypt.ErrHashTooShort) {
@@ -82,6 +70,7 @@ func (as *AuthService) StaffSignUp(ctx context.Context, req *pb.StaffSignUpReque
 		as.logger.For(ctx).Error("username is already signed up", zap.Error(err))
 		return nil, status.Error(codes.AlreadyExists, "user is already signed up")
 	}
+	as.logger.For(ctx).Info("StaffSignUp finished", zap.String("PhoneNumber", req.PhoneNumber), zap.String("Username", req.Username))
 
 	return &res, nil
 }
