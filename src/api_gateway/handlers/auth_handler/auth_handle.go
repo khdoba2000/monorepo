6cec40ec80443dbe9c6dabb89fce8d3befc941bf
NeverlandMJ <khasanovasumbula@gmail.com>
2022-11-11 15:46:38 +0500

    reset password handler for stuff

--- src/api_gateway/handlers/auth_handler/auth_handle.go
+++ src/api_gateway/handlers/auth_handler/auth_handle.go
@@ -2,13 +2,16 @@ package auth_handler
 
 import (
 	"context"
+	"fmt"
 	"monorepo/src/api_gateway/configs"
 	"monorepo/src/api_gateway/dependencies"
 	"monorepo/src/api_gateway/models"
 	"monorepo/src/api_gateway/utils"
 	u "monorepo/src/api_gateway/utils"
 	"monorepo/src/idl/auth_service"
+	"monorepo/src/libs/etc"
 	"monorepo/src/libs/log"
+	libsUtils "monorepo/src/libs/utils"
 	"net/http"
 	"time"
 
@@ -94,4 +97,30 @@ func (ah *authHandler) StuffLogin(w http.ResponseWriter, r *http.Request) {
 		RefreshToken: tokens.Refresh,
 	})
 
+}
+
+func (ah *authHandler) ResetPassword(w http.ResponseWriter, r *http.Request) {
+	// read body from request
+	var body models.ReqResetPassword
+	if err := u.BodyParser(r, body); err != nil {
+		u.HandleBadRequestErrWithMessage(w, err, "invalid body")
+		return
+	}
+
+	// check if the password meets requirements
+	if err := libsUtils.ValidatePassword(body.NewPassword); err != nil {
+		u.HandleBadRequestResponse(w, err.Error())
+		return
+	}
+
+	hashedPassword, err := etc.GeneratePasswordHash(body.NewPassword)
+	if err != nil {
+		u.HandleInternalWithMessage(w, err, "error occured whiling hashing new password")
+	}
+
+	// this is just to avoid unused variable error. Later we will send it to service where this hashed password will be saved to database
+	fmt.Println(hashedPassword)
+	// here we should update password from databse
+
+	w.WriteHeader(http.StatusOK)
 }
